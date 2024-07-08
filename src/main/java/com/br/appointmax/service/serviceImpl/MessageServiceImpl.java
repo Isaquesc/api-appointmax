e37e949287166a4bf998cb01c36f112f62ed09d2
Isaque Costa <isaquedeco@gmail.com>
2024-07-08 00:48:23 -0300

    refatorando a classe da mensageService

--- src/main/java/com/br/appointmax/service/serviceImpl/MessageServiceImpl.java
+++ src/main/java/com/br/appointmax/service/serviceImpl/MessageServiceImpl.java
@@ -5,6 +5,10 @@ import com.br.appointmax.model.Message;
 import com.br.appointmax.repository.ClientRepository;
 import com.br.appointmax.repository.MessageRepository;
 import com.br.appointmax.service.MessageService;
+import com.br.appointmax.service.WhatsAppService;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,25 +17,63 @@ import java.sql.Timestamp;
 @Service
 public class MessageServiceImpl implements MessageService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageServiceImpl.class);
+
+    private static final String STATUS_PENDING = "PENDING";
+    private static final String STATUS_SENT = "SUCESS";
+    private static final String STATUS_FAILED = "FAILED";
+
     @Autowired
     private ClientRepository clientRepository;
 
     @Autowired
     private MessageRepository messageRepository;
 
+    @Autowired
+    private WhatsAppService whatsAppService;
+
     //TODO add template kafka
 
-    public void sendMessage(Long clientId, String messageContent) {
-        Client client = clientRepository.findById(clientId).orElseThrow(() -> new RuntimeException("Client not found"));
+//    @Transactional
+    @Override
+    public String sendMessage(Long clientId, String messageContent) {
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+
+        String personalizedMessage = personalizeMessage(messageContent, client);
 
         //TODO ajustar parametros de mensagem
         Message message = new Message();
         message.setClient(client);
-        message.setContent(messageContent);
-        message.setStatus("PENDING");
+        message.setContent(personalizedMessage);
+        message.setStatus(STATUS_PENDING);
         message.setTimestamp(new Timestamp(System.currentTimeMillis()));
         messageRepository.save(message);
-        //kafkaTemplate.send("messages", messageContent);
+
+        try {
+            whatsAppService.sendWhatsAppMessage("+55" + client.getPhone(), message.getContent());
+            message.setStatus(STATUS_SENT);
+            messageRepository.save(message);
+            logger.info("Message sent successfully to {}", client.getPhone());
+
+            return "Message sent successfully to " + client.getPhone();
+        } catch (Exception e) {
+            message.setStatus(STATUS_FAILED);
+            messageRepository.save(message);
+            logger.error("Failed to send message to {}", client.getPhone(), e);
+
+            return "Failed to send message to " + client.getPhone();
+        }
+
+        //        kafkaTemplate.send("messages", messageContent);
+    }
+
+    private String personalizeMessage(String messageContent, Client client) {
+        // Implementação básica de substituição de variáveis
+        String personalizedMessage = messageContent
+                .replace("{nome}", client.getName());
+
+        return personalizedMessage;
     }
 
 }
