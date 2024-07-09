f1ccddb55bfbd97c1f1eae9fba7e423ab9390faa
Isaque Costa <isaquedeco@gmail.com>
2024-07-09 13:04:11 -0300

    adicionando constants para mapeando da classe CSVService

---
+++ src/main/java/com/br/appointmax/constants/CSVServiceConstants.java
@@ -0,0 +1,15 @@
+package com.br.appointmax.Constants;
+
+import java.util.regex.Pattern;
+
+public class CSVServiceConstants {
+
+    public static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    public static final Pattern PHONE_PATTERN = Pattern.compile("^\\+?[0-9]{10,15}$");
+
+    public static final String CSV_UPLOAD_SUCCESS = "CSV upload successful";
+    public static final String CSV_UPLOAD_IO_ERROR = "Failed to upload CSV due to an IO error";
+    public static final String CSV_UPLOAD_VALIDATION_ERROR = "Failed to upload CSV due to a validation error";
+    public static final String INVALID_DATA_WARNING = "Invalid data in line: {}";
+
+}
